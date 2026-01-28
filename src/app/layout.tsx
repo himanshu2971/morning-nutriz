45cca427aa38cc6499b0c3288a63c93eeb4006ab
Jarvis <himanshushourabh@gmail.com>
2026-01-28 11:48:06 +0530

    Fixed Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import type { Metadata } from "next";
 import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["600", "700", "800"],
